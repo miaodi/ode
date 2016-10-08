0fa20cdd0efbc2929935110ddcc46d4dbb975de1
dimiao <miaodi1987@gmail.com>
2016-10-08 10:00:18 -0400

    add Runge-Kutta

--- RungeKuttaSolver.cpp
+++ RungeKuttaSolver.cpp
@@ -8,10 +8,12 @@ double *RungeKuttaSolver::SolveEquation() {
     double *out_put = new double[GetIterationNumber()+1];
     out_put[0] = GetInitialValue();
     for (int i = 1; i <= GetIterationNumber(); ++i) {
-        double k1=GetStepSize()*RightHandSide(GetInitialTime()+(i-1)*GetStepSize(),out_put[i-1]);
-        double k2=GetStepSize()*RightHandSide(GetInitialTime()+(i-1+.5)*GetStepSize(),out_put[i-1]+.5*k1);
-        double k3=GetStepSize()*RightHandSide(GetInitialTime()+(i-1+.5)*GetStepSize(),out_put[i-1]+.5*k2);
-        double k4=GetStepSize()*RightHandSide(GetInitialTime()+i*GetStepSize(),out_put[i-1]+k3);
+        double k1 = GetStepSize() * RightHandSide(out_put[i - 1], GetInitialTime() + (i - 1) * GetStepSize());
+        double k2 = GetStepSize() *
+                    RightHandSide(out_put[i - 1] + .5 * k1, GetInitialTime() + (i - 1 + .5) * GetStepSize());
+        double k3 = GetStepSize() *
+                    RightHandSide(out_put[i - 1] + .5 * k2, GetInitialTime() + (i - 1 + .5) * GetStepSize());
+        double k4 = GetStepSize() * RightHandSide(out_put[i - 1] + k3, GetInitialTime() + i * GetStepSize());
         out_put[i]=out_put[i-1]+1.0/6*(k1+2*k2+2*k3+k4);
         cout<<out_put[i-1]<<" ";
     }
--- main.cpp
+++ main.cpp
@@ -2,13 +2,13 @@
 #include "RungeKuttaSolver.h"
 
 double f(double y,double t){
-    return 1.0+t;
+    return 1.0 + t + y;
 }
 int main() {
-    RungeKuttaSolver a(100,0,1,2);
+    RungeKuttaSolver a(15, 0, 1, 2);
     a.setRHS(f);
     a.SolveEquation();
-    ForwardEulerSolver b(10,0,1,2);
+    ForwardEulerSolver b(100, 0, 1, 2);
     b.setRHS(f);
     b.SolveEquation();
     return 0;
